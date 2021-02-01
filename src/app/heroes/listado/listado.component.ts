646194ba872ae442182699e54abc43d916aa3d40
Axl Vejarano <axlvejarano@gmail.com>
2021-02-01 13:40:01 -0500

    basic

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  
+  heroes:string[] = ['Spiderman','Venom', 'Carnage', 'Duende'];
+
+  heroeBorrado:string = '';
+
+  borrarHeroe(){
+    // console.log("Borrando...");
+    this.heroeBorrado = this.heroes.shift();
+    console.log(this.heroeBorrado);
+    
+  }
+
+}
